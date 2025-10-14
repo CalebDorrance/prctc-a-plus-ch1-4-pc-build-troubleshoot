1065dc838b843e3b1f469892dd0b52a333b49a6b
Caleb Dorrance <caleb.dorrance1@gmail.com>
2025-10-14 10:53:42 -0400

    Update windows_system_inventory.ps1

--- scripts/windows_system_inventory.ps1
+++ scripts/windows_system_inventory.ps1
@@ -1,1 +1,5 @@
-
+ Get-ComputerInfo | Select-Object CsName, WindowsVersion, OsBuildNumber
+Get-CimInstance Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors
+Get-CimInstance Win32_PhysicalMemory | Select-Object Capacity, Speed, PartNumber
+Get-PhysicalDisk | Select-Object FriendlyName, MediaType, Size, HealthStatus
+Get-Disk | Select-Object Number, PartitionStyle, OperationalStatus, Size
