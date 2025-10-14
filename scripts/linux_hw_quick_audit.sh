96f34ad5ab80a3a90e490442cb0058ccbd02233e
Caleb Dorrance <caleb.dorrance1@gmail.com>
2025-10-14 10:55:10 -0400

    Update linux_hw_quick_audit.sh

--- scripts/linux_hw_quick_audit.sh
+++ scripts/linux_hw_quick_audit.sh
@@ -1,1 +1,4 @@
-
+ #!/usr/bin/env bash
+echo "=== CPU ==="; lscpu | egrep 'Model name|CPU\\(s\\)|Core|Socket'
+echo "=== Memory ==="; free -h
+echo "=== Disks ==="; lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
